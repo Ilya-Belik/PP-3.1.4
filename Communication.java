0230a989fb1683a9b0153a6a263e360831550ab3
Ilya Belik <141572840+Ateglif@users.noreply.github.com>
2023-12-17 17:05:20 +0500

    Add files via upload

---
+++ Communication.java
@@ -0,0 +1,52 @@
+package app;
+import app.configs.models.User;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class Communication {
+
+    private RestTemplate restTemplate;
+    private HttpHeaders httpHeaders;
+    private String URL ="http://94.198.50.185:7081/api/users";
+
+    public Communication(RestTemplate restTemplate, HttpHeaders httpHeaders) {
+        this.restTemplate = restTemplate;
+        this.httpHeaders = httpHeaders;
+        this.httpHeaders.set("Cookie",
+                String.join(";", restTemplate.headForHeaders(URL).get("set-Cookie")));
+    }
+    public String getAnswer() {
+        return addUser().getBody() + updateUser().getBody() + deleteUser().getBody();
+    }
+    private List<User> getAllUsers() {
+        ResponseEntity<List<User>> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() { });
+        System.out.println(responseEntity.getHeaders());
+        return responseEntity.getBody();
+    }
+    private ResponseEntity<String> addUser() {
+        User user = new User(3L, "James", "Brown", (byte) 15);
+        HttpEntity<User> entity = new HttpEntity<>(user, httpHeaders);
+        return restTemplate.postForEntity(URL, entity, String.class);
+    }
+    private ResponseEntity<String> updateUser() {
+        User user = new User(3L, "Thomas", "Shelby", (byte) 45);
+        HttpEntity<User> entity = new HttpEntity<>(user, httpHeaders);
+        return restTemplate.exchange(URL, HttpMethod.PUT, entity, String.class, 3);
+    }
+    private ResponseEntity<String> deleteUser() {
+        Map<String, Long> uriVariables = new HashMap<String, Long>() {{
+            put("id", 3L);
+        }};
+        HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
+        return restTemplate.exchange(URL + "/{id}", HttpMethod.DELETE, entity, String.class, uriVariables);
+    }
+}
